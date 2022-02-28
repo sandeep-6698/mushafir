cb27b75b55febe46e64e661c6ead1ee78cfa4386
sandeep-6698 <sandeep.kumar@taknikbharti.com>
2022-02-28 20:47:33 +0530

    fixed navbar

--- views/common/navbar.php
+++ views/common/navbar.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-light bg-light">
   <a class="navbar-brand" href="<?php echo BASE_URL."/dashboard"?>"><b>Musafir</b></a>
   <div class="ml-auto d-flex align-items-center">
-    <?php if(Session::get('user')): 
-      if(Session::get('user')['role'] == 'admin'):
+    <?php if(Session::get('user')){
+      if(Session::get('user')['role'] == 'admin'){
       ?>
       <div class="m-0 mr-2 rounded-circle flow-hidden">
         <a class="p-0" href=<?php echo BASE_URL."/admin/dashboard" ?>>
           <img height="50px" width="50px" src="<?php echo STATIC_FILES.Session::get('user')['pf_image']?>"/>
         </a>  
       </div>
-      <? endif; ?>
+      <?php } ?>
     <a class="btn" href=<?php echo BASE_URL."/auth/logout" ?>>Logout</a>
-    <?php else: 
+    <?php } else{
       $redirect = in_array(Request::get('url'), ['auth/login', 'auth/register']) ? 'dashboard' : Request::get('url');
       ?>
     <a class="btn" href=<?php echo BASE_URL."/auth/login?redirect=/$redirect"?>>Login</a>
-    <?php endif; ?>
+    <?php } ?>
   </div>
 </nav>
